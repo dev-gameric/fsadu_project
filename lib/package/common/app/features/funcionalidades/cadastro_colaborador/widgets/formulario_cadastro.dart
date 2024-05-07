469d1e9898edf5338edebba1611f3f825cb72ee3
Eric Silva (gameric) <eric.silva.de.souza.93.com@gmail.com>
2024-05-07 15:35:09 -0300

    add campos de form

--- lib/package/common/app/features/funcionalidades/cadastro_colaborador/widgets/formulario_cadastro.dart
+++ lib/package/common/app/features/funcionalidades/cadastro_colaborador/widgets/formulario_cadastro.dart
@@ -33,7 +33,7 @@ class _FormularioCadastroState extends State<FormularioCadastro> {
   final seccaoController = TextEditingController();
   final agenciaController = TextEditingController();
   final digitoAgenciaController = TextEditingController();
-  final contaController = TextEditingController();
+  final contaBancoController = TextEditingController();
   final digitoContaController = TextEditingController();
 
   @override
@@ -58,168 +58,205 @@ class _FormularioCadastroState extends State<FormularioCadastro> {
           ),
           height: altura,
           width: largura * 0.9,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Form(
-                key: chaveFormulario,
-                child: Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'dataNasc',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'Data Nascimento',
-                        editingController: dataNascController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                      Texto.simples('Nome Completo(*)', corTexto: preto),
-                      Formulario(
-                        chave: 'nome',
-                        icone: Icons.accessibility_new_sharp,
-                        tipo: 'nome',
-                        editingController: nomeController,
-                      ),
-                    ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Form(
+                  key: chaveFormulario,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Texto.simples('Nome Completo(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira Nome Completo',
+                          chave: 'nome',
+                          tipo: '',
+                          editingController: nomeController,
+                        ),
+                        Texto.simples('Data De Nascimento(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.datetime,
+                          hint: 'Insira Data de Nascimento',
+                          chave: 'dataNasc',
+                          tipo: '',
+                          editingController: dataNascController,
+                        ),
+                        Texto.simples('CPF(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira seu CPF',
+                          keyboard: TextInputType.number,
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: cpfController,
+                        ),
+                        Texto.simples('PIS(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira seu PIS',
+                          keyboard: TextInputType.number,
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: pisController,
+                        ),
+
+                        //dropbutton SEXO
+
+                        Texto.simples('RG(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.number,
+                          hint: 'Insira seu RG',
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: rgController,
+                        ),
+                        Texto.simples('RG Orgão Emissor(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira Orgão Emissor',
+                          chave: 'nome',
+                          tipo: '',
+                          editingController: orgaoEmissorController,
+                        ),
+                        Texto.simples('Endereço(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira seu Endereço',
+                          chave: 'nome',
+                          tipo: '',
+                          editingController: enderecoController,
+                        ),
+                        Texto.simples('Bairro(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira Seu Bairro',
+                          chave: 'nome',
+                          tipo: '',
+                          editingController: bairroController,
+                        ),
+                        Texto.simples('Complemento(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira seu Complemento',
+                          chave: 'nome',
+                          tipo: '',
+                          editingController: complementoController,
+                        ),
+                        Texto.simples('Cidade(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira Sua Cidade',
+                          chave: 'nome',
+                          tipo: '',
+                          editingController: cidadeController,
+                        ),
+                        Texto.simples('CEP(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira Seu CEP',
+                          keyboard: TextInputType.number,
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: cepController,
+                        ),
+                        Texto.simples('UF(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira UF',
+                          chave: 'nome',
+                          tipo: '',
+                          editingController: ufController,
+                        ),
+                        Texto.simples('E-mail(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.emailAddress,
+                          hint: 'Insira Seu E-mail',
+                          chave: 'email',
+                          tipo: '',
+                          editingController: emailController,
+                        ),
+                        Texto.simples('Celular(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.number,
+                          hint: 'Insira Seu Número Celular',
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: celularController,
+                        ),
+                        Texto.simples('Certificado Reservista',
+                            corTexto: preto),
+                        Formulario(
+                          hint: 'Insira seu Nº Reservista',
+                          keyboard: TextInputType.number,
+                          obrigatorio: false,
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: certficadoReservistaController,
+                        ),
+                        Texto.simples('Título de Eleitor(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.number,
+                          hint: 'Insira o Nº do Título De Eleitor',
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: tituloEleitoController,
+                        ),
+                        Texto.simples('Zona(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.number,
+                          hint: 'Insira a Zona',
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: zonaController,
+                        ),
+                        Texto.simples('Secção(*)', corTexto: preto),
+                        Formulario(
+                          hint: 'Insira a Secção',
+                          keyboard: TextInputType.number,
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: seccaoController,
+                        ),
+
+                        // 2 Dropdown button
+                        // Banco
+                        // tipo do banco
+
+                        Texto.simples('Agência(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.number,
+                          hint: 'Insira Sua Agência Bancária',
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: agenciaController,
+                        ),
+                        Texto.simples('Dígito da Agência(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.number,
+                          hint: 'Insira o Dígito de Sua Agência',
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: digitoAgenciaController,
+                        ),
+                        Texto.simples('Conta(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.number,
+                          hint: 'Insira Sua Conta Bancária',
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: contaBancoController,
+                        ),
+                        Texto.simples('Dígito da Conta(*)', corTexto: preto),
+                        Formulario(
+                          keyboard: TextInputType.number,
+                          hint: 'Insira o Dígito da Sua Conta Bancária',
+                          chave: 'numero',
+                          tipo: '',
+                          editingController: digitoContaController,
+                        ),
+
+                        //DropDown Button
+                        //Escolaridade
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
